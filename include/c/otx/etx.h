3ab1e1310f3ef3204da0d43530bf95f9376cac9a
Srđan Milaković <sm108@rice.edu>
2022-09-13 23:25:09 -0500

    Removed C API.

--- include/c/otx/etx.h
+++
@@ -1,6 +0,0 @@
-/* LICENSE PLACEHOLDER */
-
-#ifndef OTX_ETX_H
-#define OTX_ETX_H
-
-#endif //OTX_ETX_H
--- include/c/otx/otx.h
+++
@@ -1,9 +0,0 @@
-/* LICENSE PLACEHOLDER */
-
-#ifndef OTX_OTX_H
-#define OTX_OTX_H
-
-#include "atx.h"
-#include "etx.h"
-
-#endif //OTX_OTX_H
